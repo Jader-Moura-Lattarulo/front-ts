2dbfcbff0706c140358ffe5cb2b4b6bc199c8048
Jader Moura <jader.dev.moura@gmail.com>
2024-12-09 13:05:58 -0300

    update task list

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
+interface TodoItem {
+  id: string,
+  text: string,
+  completed: boolean
+}
+
 function App() {
-  const [count, setCount] = useState(0)
+  //Keys
+  const keyTaksMemory: string = "tasks"
+
+  //States
+  const [todos, setTodos] = useState<TodoItem[]>([])
+  const [newTodo, setNewTodo] = useState<string>("")
+  const [isLoaded, setIsLoaded] = useState<boolean>(false) //Buscar dados da memória 
+  
+  // Functions
+  const addTodo = (): void => {
+    if(newTodo !== "") {
+      const newId = crypto.randomUUID()
+      const newTodoItem: TodoItem = {
+        id: newId,
+        text: newTodo,
+        completed: false
+      }
+      setTodos([...todos, newTodoItem])
+      setNewTodo("")
+    }
+  }
+
+  const removeTask = (id:string): void => {
+    const updatedTasks = todos.filter((todo) => todo.id !== id)
+    setTodos(updatedTasks)
+  }
+
+  const markDone = (id: string): void => {
+    const updatedTodos = todos.map((todo) => {
+      if (todo.id === id) {
+        return { ...todo, completed: !todo.completed }
+      }
+      return todo
+    })
+    setTodos(updatedTodos)
+  }
+
+  const getCompletedTasks = (): TodoItem[] => {
+    return todos.filter(todo => todo.completed)
+  }
+
+  useEffect(() => {
+    if(isLoaded) {
+      localStorage.setItem(keyTaksMemory, JSON.stringify(todos))
+    }
+  }, [todos, isLoaded])
+
+  useEffect(() => {
+    const tasksFromMemory = localStorage.getItem(keyTaksMemory)
+
+    if(tasksFromMemory) {
+      setTodos(JSON.parse(tasksFromMemory))
+    }
+
+    setIsLoaded(true)
+
+  }, [])
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='app'>
+        <div className='container'>
+          <h1>Lista de Tarefas - {getCompletedTasks().length} / {todos.length}</h1>
+          <div className='input-container'>
+            <input type="text" value={newTodo} onChange={(e) => setNewTodo(e.target.value)} />
+            <button onClick={addTodo}>Adicionar Tarefa</button>
+          </div>
+          <ol>
+            {
+              todos.map((todo) => (
+                <li key={todo.id}>
+                  <input type="checkbox" checked={todo.completed} onChange={() => markDone(todo.id)} />
+                  <span style={{ textDecoration: todo.completed ? 'line-through' : 'none'}}>{todo.text}</span>
+                  <button onClick={() => removeTask(todo.id)}>Remover</button>
+                </li>
+              ))
+            }
+          </ol>
+          <button onClick={console.log}>
+            Alterar Tema
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
